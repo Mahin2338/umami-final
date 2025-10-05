bcc0a1a68cd8315f32c62a2d33b72953fecd2d3f
Mahdi <mahdi.moha@hotmail.com>
2025-10-05 11:00:05 +0100

    create new remote statefile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "ecs-umami-terraform-state"
-    key     = "ecs-final-project/terraform.tfstate"
-    region  = "eu-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt = true
-    
+    bucket         = "ecs-umami-terraform-state-new"
+    key            = "ecs-final-project/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-locks-new"
+    encrypt        = true
   }
 }
